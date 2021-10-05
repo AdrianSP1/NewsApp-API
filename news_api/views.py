28de7e8a6d59152bf50964ed335d027c5011c1fa
xiod <adriangustavose@gmail.com>
2021-10-05 19:24:03 -0300

    conexion urls.py create a conexion defHome url api- create a template with information extracted by apinews

--- news_api/views.py
+++ news_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+API_KEY = '89253bef34204bfa83812b218dc4553e'
+
+def home(request):
+    url = f'https://newsapi.org/v2/everything?q=tesla&from=2021-09-05&sortBy=publishedAt&apiKey={API_KEY}'
+    response =requests.get(url)
+    data = response.json()
+    
+    articles = data['articles']
+
+    context={
+        'articles':articles
+    }
+
+    return render(request, 'news_api/home.html', context)
